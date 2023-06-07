f2ceff88a617f43b2d3d05084446d84056438658
Marcos Dolce <marcosdolce.91@gmail.com>
2023-06-07 11:32:57 -0300

    index.js actualizado
    Modifiqué el archivo index.js para que funcione bajo la versión actual de node.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'typeface-roboto';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Since scr/index.js file is the entry point we need to tweak it a bit so that the react dom loads after the “deviceready” event has been fired by cordova.
+if (window.cordova) {
+  document.addEventListener('deviceready', () => {
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }, false);
+} else {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
