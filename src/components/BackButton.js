adc518d43b7d58477394daa5c3e1912f8dc57c97
Marcos Dolce <marcosdolce.91@gmail.com>
2023-09-03 17:21:07 -0300

    Upgraded BackButton Ingredients

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import './BackButton.css';
 
-class BackButton extends React.Component {
-    handleClick() {
+function BackButton() {
+    const handleClick = () => {
         window.history.back();
     }
 
-    render() {
-        return (
-            <img
-                src="images/icons/back-button.png"
-                className="backButton"
-                onClick={() => this.handleClick()} 
-                alt="Back"
-            >
-            </img>
-        )
-    } 
+    return (
+        <img
+            src="images/icons/back-button.png"
+            className="backButton"
+            onClick={() => handleClick()}
+            alt="Back"
+        >
+        </img>
+    )
 }
 
 export default BackButton;
--- src/components/Ingredients.js
+++ src/components/Ingredients.js
@@ -1,11 +1,11 @@
 // Import Components
 import React from "react";
 
-class Ingredients extends React.Component {
-    
+function Ingredients({ value, ingredients, onChange }) {
+
     /* When the user clicks on the button, 
     toggle between hiding and showing the dropdown content */
-    collapsible(e) {
+    const collapsible = (e) => {
         let drop = document.getElementById(e.target.id);
         let content = drop.nextElementSibling;
         if (content.style.maxHeight) {
@@ -15,48 +15,46 @@ class Ingredients extends React.Component {
         }
     }
 
-    render() {
-        return (
-            <div className="fridge-container">
-                {this.props.ingredients.sort((a, b) => a.name.localeCompare(b.name)).map((item, i) => {
-                    return (
-                        <div key={"1" + i} className="dropdown">
-                            <div id={"drop" + i} onClick={(e) => this.collapsible(e)} className="dropbtn">
-                                <div id={"drop" + i} className="image-dropdown" style={{ backgroundImage: item.image }}></div>
-                                <div id={"drop" + i} className="text-dropdown">{item.name}</div>
-                                <div id={"drop" + i} className="arrow-dropdown" style={{ backgroundImage: "url(images/fridge/Arrow.png)" }}></div>
-                            </div>
-                            <div id="collapsible" className="dropdown-content">
-                                {item.ingredients.sort().map((ingredient, i, arr) => {
-                                    return (
-                                        <div key={"C1" + i} className="light">
-                                            <label>
-                                                <span name={ingredient} className="name-label">{ingredient}</span>
-                                                <input
-                                                    type="checkbox"
-                                                    id={ingredient}
-                                                    name={ingredient}
-                                                    value={ingredient}
-                                                    className="input-checkbox"
-                                                    onChange={() => this.props.onChange(ingredient)}
-                                                    checked={this.props.value.includes(ingredient)}
-                                                ></input>
-                                                <span className="design"></span>
-                                                <span className="text-ingredients" name={ingredient}>{ingredient}</span>
-
-                                            </label>
-                                            {i < arr.length - 1 ? <hr className="line"></hr> : null}
-                                        </div>
-                                    )
-                                })
-                                }
-                            </div>
+    return (
+        <div className="fridge-container">
+            {ingredients.sort((a, b) => a.name.localeCompare(b.name)).map((item, i) => {
+                return (
+                    <div key={"1" + i} className="dropdown">
+                        <div id={"drop" + i} onClick={(e) => this.collapsible(e)} className="dropbtn">
+                            <div id={"drop" + i} className="image-dropdown" style={{ backgroundImage: item.image }}></div>
+                            <div id={"drop" + i} className="text-dropdown">{item.name}</div>
+                            <div id={"drop" + i} className="arrow-dropdown" style={{ backgroundImage: "url(images/fridge/Arrow.png)" }}></div>
                         </div>
-                    )
-                })}
-            </div>
-        );
-    }
+                        <div id="collapsible" className="dropdown-content">
+                            {item.ingredients.sort().map((ingredient, i, arr) => {
+                                return (
+                                    <div key={"C1" + i} className="light">
+                                        <label>
+                                            <span name={ingredient} className="name-label">{ingredient}</span>
+                                            <input
+                                                type="checkbox"
+                                                id={ingredient}
+                                                name={ingredient}
+                                                value={ingredient}
+                                                className="input-checkbox"
+                                                onChange={() => onChange(ingredient)}
+                                                checked={value.includes(ingredient)}
+                                            ></input>
+                                            <span className="design"></span>
+                                            <span className="text-ingredients" name={ingredient}>{ingredient}</span>
+
+                                        </label>
+                                        {i < arr.length - 1 ? <hr className="line"></hr> : null}
+                                    </div>
+                                )
+                            })
+                            }
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    );
 }
 
 export default Ingredients;
