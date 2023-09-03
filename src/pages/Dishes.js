5e3d818ab43f5aeaec449397da5a68e4c1b24c0d
Marcos Dolce <marcosdolce.91@gmail.com>
2023-09-03 17:28:42 -0300

    Upgraded Dishes

--- src/pages/Dishes.js
+++ src/pages/Dishes.js
@@ -2,36 +2,33 @@ import React from 'react';
 import DishesList from "../components/DishesList";
 import BackButton from "../components/BackButton";
 
-class Dishes extends React.Component {
-    render() {
+function Dishes({ ingredients, dishes, selectDish, markAsFavorite, handleInputChange, resetIngredients }) {
 
-        const ingredients = this.props.ingredients.map(item => {
-            return (
-                <div key={item} className="card-text" onClick={() => this.props.handleInputChange(item)} >{item}
-                    <div style={{ backgroundImage: "url(images/icons/delete-icon-x.png)" }} className="delete-icon"></div>
-                </div>
-            );
-        });
-
+    const ingredients = ingredients.map(item => {
         return (
-            <div className="main-div" style={{ paddingBottom: "40%" }}>
-                <BackButton></BackButton>
-                <div className="title">Mi Refrigerador</div>
-                <div className="main-container">
-                    <img src="images/icons/delete-icon.svg" alt="delete" className="delete-image" onClick={() => this.props.resetIngredients()}></img>
-                    <div className="card-dishes">
-                        {ingredients}
-                    </div>
-                    <DishesList
-                        dishes={this.props.dishes}
-                        selectDish={(sel) => this.props.selectDish(sel)}
-                        markAsFavorite={(fav) => this.props.markAsFavorite(fav)}
-                    ></DishesList>
-                </div>
+            <div key={item} className="card-text" onClick={() => handleInputChange(item)} >{item}
+                <div style={{ backgroundImage: "url(images/icons/delete-icon-x.png)" }} className="delete-icon"></div>
             </div>
-        )
+        );
+    });
 
-    }
+    return (
+        <div className="main-div" style={{ paddingBottom: "40%" }}>
+            <BackButton></BackButton>
+            <div className="title">Mi Refrigerador</div>
+            <div className="main-container">
+                <img src="images/icons/delete-icon.svg" alt="delete" className="delete-image" onClick={() => resetIngredients()}></img>
+                <div className="card-dishes">
+                    {ingredients}
+                </div>
+                <DishesList
+                    dishes={dishes}
+                    selectDish={(sel) => selectDish(sel)}
+                    markAsFavorite={(fav) => markAsFavorite(fav)}
+                ></DishesList>
+            </div>
+        </div>
+    )
 }
 
 export default Dishes;
